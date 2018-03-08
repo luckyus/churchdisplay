4108dfc4d1f48f377db95231e14dcb3f6bf025a6
LuckyUs <briandreleung@gmail.com>
2018-03-08 19:00:29 +0800

    sensor

---
+++ plugins/DHT22SensorPlugin.js
@@ -0,0 +1,69 @@
+var model = require('./../../resources/model');
+var utils = require('./../../utils/utils.js');
+
+var interval, sensor;
+var pluginName = 'Temperature & Humidity';
+var localParams = {
+	'simulate': false,
+	'frequency': 5000
+};
+
+exports.start = function(params) {
+	localParams = params;
+
+	// debug
+	console.log('simulate: %s...', localParams.simulate);
+
+	if (params.simulate) {
+		simulate();
+	} else {
+		connectHardware();
+	}
+};
+
+exports.stop = function() {
+	if (params.simulate) {
+		clearInterval(interval);
+	} else {
+		sensor.unexport();
+	}
+	console.info('%s plugin stopped!', pluginName);
+};
+
+function connectHardware() {
+	var sensorDriver = require('node-dht-sensor');
+	var sensor = {
+		initialize: function() {
+			return sensorDriver.initialize(22, model.temperature.gpio);
+		},
+		read: function() {
+			var readout = sensorDriver.read();
+			model.temperature.value = parseFloat(readout.temperature.toFixed(2));
+			model.humidity.value = parseFloat(readout.humidity.toFixed(2));
+			// showValue();
+
+			setTimeout(function() {
+				sensor.read();
+			}, localParams.frequency);
+		}
+	};
+	if (sensor.initialize()) {
+		console.info('Hardware %s sensor started!', pluginName);
+		sensor.read();
+	} else {
+		console.warn('Failed to initialize sensor!');
+	}
+}
+
+function simulate() {
+	interval = setInterval(function() {
+		model.temperature.value = utils.randomInt(0, 40);
+		model.humidity.value = utils.randomInt(0, 100);
+		showValue();
+	}, localParams.frequency);
+	console.info('Simulated %s sensor started!', pluginName);
+}
+
+function showValue() {
+	console.info('Temperature: %s C, humidity %s \%', model.temperature.value, model.humidity.value);
+}
