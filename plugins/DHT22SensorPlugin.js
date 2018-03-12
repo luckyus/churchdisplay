7faa4c816a7843f84eb233c72e2042f41cc1e396
LuckyUs <briandreleung@gmail.com>
2018-03-12 15:46:56 +0800

    fix require json

--- plugins/DHT22SensorPlugin.js
+++ plugins/DHT22SensorPlugin.js
@@ -1,35 +1,5 @@
-var model = {
-	"name": "Church Outdoor Display",
-	"description": "Hop Yat Church's Outdoor Display with Sensors",
-	"port": 3080,
-	"temperature": {
-		"name": "temperature",
-		"gpio": 12,
-		"value": 0,
-		"unit": "celsius"
-	},
-	"humidity": {
-		"name": "humidity",
-		"gpio": 12,
-		"value": 0,
-		"unit": "%"
-	},
-	"fan": {
-		"name": "fan",
-		"gpio": 0,
-		"value": true
-	},
-	"led": {
-		"name": "led",
-		"gpio": 4,
-		"value": true
-	},
-	"lcd": {
-		"name": "lcd",
-		"gpio": 0,
-		"value": "iGuardPayroll.com"
-	}
-};
+var path = require('path');
+var model = require(path.resolve(__dirname, './model.json'));
 
 var interval, sensor;
 var pluginName = 'Temperature & Humidity';
