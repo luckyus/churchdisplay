10aa55c26318ac7f097c85f55ff8699fd4350e52
LuckyUs <briandreleung@gmail.com>
2018-03-12 15:38:42 +0800

    asdfa

--- plugins/DHT22SensorPlugin.js
+++ plugins/DHT22SensorPlugin.js
@@ -1,4 +1,35 @@
-var model = require('./model.json');
+var model = {
+	"name": "Church Outdoor Display",
+	"description": "Hop Yat Church's Outdoor Display with Sensors",
+	"port": 3080,
+	"temperature": {
+		"name": "temperature",
+		"gpio": 12,
+		"value": 0,
+		"unit": "celsius"
+	},
+	"humidity": {
+		"name": "humidity",
+		"gpio": 12,
+		"value": 0,
+		"unit": "%"
+	},
+	"fan": {
+		"name": "fan",
+		"gpio": 0,
+		"value": true
+	},
+	"led": {
+		"name": "led",
+		"gpio": 4,
+		"value": true
+	},
+	"lcd": {
+		"name": "lcd",
+		"gpio": 0,
+		"value": "iGuardPayroll.com"
+	}
+};
 
 var interval, sensor;
 var pluginName = 'Temperature & Humidity';
