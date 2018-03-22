d530d529aa565a3cbb1d3355ad0e1be2161d9df2
LuckyUs <briandreleung@gmail.com>
2018-03-22 17:35:20 +0800

    640x480

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ var fullpathCamera = folderCamera + '/' + fileCamera;
 var camera = require('./plugins/camera');
 // camera.start({ mode: 'timelapse', output: fullpath, t: 60000, tl: 10 });
 // camera.start(['-o', fullpathCamera, '-t', Number.MAX_SAFE_INTEGER.toString(), '-tl', '100']);
-camera.start(['-o', fullpathCamera, '-vf', '-n', '-t', '999999999', '-tl', '10']);
+camera.start(['-o', fullpathCamera, '-vf', '-n', '-md', '7', '-t', '999999999', '-tl', '500']);
 
 app.use(express.static(path.resolve(__dirname, 'public')));
 
